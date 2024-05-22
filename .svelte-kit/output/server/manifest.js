0b76ea2a0d383277c86e65e73a3a512b347e92f2
Salma Abdel Magid <sabdelmagid>
2024-05-22 13:55:49 -0700

    bassem

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -1,10 +1,10 @@
 export const manifest = {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set([".nojekyll","android-chrome-192x192.png","android-chrome-512x512.png","apple-touch-icon.png","bok.jpg","favicon-16x16.png","favicon-32x32.png","favicon.ico","profile.png","profile.svg","psudo_teaser.jpg","psudo_teaser.png","psudo_website_teaser.jpg","riadh_circle.ico:Zone.Identifier","scope2screen_high_res.png","site.webmanifest","vimo-teaser-02.png","warchol_cv.pdf"]),
-	mimeTypes: {".png":"image/png",".jpg":"image/jpeg",".ico":"image/vnd.microsoft.icon",".svg":"image/svg+xml",".webmanifest":"application/manifest+json",".pdf":"application/pdf"},
+	assets: new Set([".nojekyll","OLDprofile.png","OLDprofile.svg","android-chrome-192x192.png","android-chrome-512x512.png","apple-touch-icon.png","bok.jpg","favicon-16x16.png","favicon-32x32.png","favicon.ico","profile.png","psudo_teaser.jpg","psudo_teaser.png","psudo_website_teaser.jpg","riadh_circle.ico:Zone.Identifier","scope2screen_high_res.png","site.webmanifest","vimo-teaser-02.png","warchol_cv.pdf"]),
+	mimeTypes: {".png":"image/png",".svg":"image/svg+xml",".jpg":"image/jpeg",".ico":"image/vnd.microsoft.icon",".webmanifest":"application/manifest+json",".pdf":"application/pdf"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.67f1abab.js","app":"_app/immutable/entry/app.ecbf81c4.js","imports":["_app/immutable/entry/start.67f1abab.js","_app/immutable/chunks/index.474b34d0.js","_app/immutable/chunks/singletons.e0ddbd6c.js","_app/immutable/entry/app.ecbf81c4.js","_app/immutable/chunks/index.474b34d0.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.fcee028f.js","app":"_app/immutable/entry/app.22735086.js","imports":["_app/immutable/entry/start.fcee028f.js","_app/immutable/chunks/index.474b34d0.js","_app/immutable/chunks/singletons.fb47f914.js","_app/immutable/entry/app.22735086.js","_app/immutable/chunks/index.474b34d0.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			() => import('./nodes/0.js'),
 			() => import('./nodes/1.js')
--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -2,6 +2,6 @@
 
 export const index = 1;
 export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.eee5299d.js","_app/immutable/chunks/index.474b34d0.js","_app/immutable/chunks/stores.a7bc094b.js","_app/immutable/chunks/singletons.e0ddbd6c.js"];
+export const imports = ["_app/immutable/nodes/1.41cf3910.js","_app/immutable/chunks/index.474b34d0.js","_app/immutable/chunks/stores.d55c04af.js","_app/immutable/chunks/singletons.fb47f914.js"];
 export const stylesheets = [];
 export const fonts = [];
