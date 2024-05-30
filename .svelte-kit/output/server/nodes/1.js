26d1462c6842963e6f857f87774ecb3375154cfe
Simon Warchol <simonwarchol@g.harvard.edu>
2024-05-30 15:15:24 +0200

    Adding meta tags

--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -2,6 +2,6 @@
 
 export const index = 1;
 export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.41ee8826.js","_app/immutable/chunks/index.474b34d0.js","_app/immutable/chunks/stores.d641e151.js","_app/immutable/chunks/singletons.637728aa.js"];
+export const imports = ["_app/immutable/nodes/1.9e84269d.js","_app/immutable/chunks/index.474b34d0.js","_app/immutable/chunks/stores.c0892cab.js","_app/immutable/chunks/singletons.cda15f71.js"];
 export const stylesheets = [];
 export const fonts = [];
